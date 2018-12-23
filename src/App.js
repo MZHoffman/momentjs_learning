33de9d6442b069cb3e37e81b5dbf9943dc2c090b
Michal Hoffman <michal.z.hoffman@gmail.com>
2018-12-23 19:54:37 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,99 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import moment from 'moment'
 
 class App extends Component {
   render() {
+    function timestampToTime(timestamp) {
+      let date = new Date(timestamp * 1000)
+      console.log(date)
+
+      let year = date.getFullYear()
+      let month = date.getMonth() + 1
+      let day = date.getDate()
+      let hours = '0' + date.getHours()
+      let minutes = '0' + date.getMinutes()
+      let formattedTime =
+        year +
+        '-' +
+        month +
+        '-' +
+        day +
+        ' ' +
+        hours.substr(-2) +
+        ':' +
+        minutes.substr(-2)
+      return formattedTime
+    }
+    function timestampToTimeUTC(timestamp) {
+      let date = new Date(timestamp * 1000)
+      console.log(new Date(timestamp * 1000))
+      let year = date.getUTCFullYear()
+      let month = date.getUTCMonth() + 1
+      let day = date.getUTCDate()
+      let hours = '0' + date.getUTCHours()
+      let minutes = '0' + date.getUTCMinutes()
+      let formattedTime =
+        year +
+        '-' +
+        month +
+        '-' +
+        day +
+        ' ' +
+        hours.substr(-2) +
+        ':' +
+        minutes.substr(-2)
+      return formattedTime
+    }
+    const timestamp = 3966000
+
+    const currentTime = moment().format('HH:mm:ss')
+    const day = moment.unix(timestamp)
+    const dayJS = timestampToTime(timestamp)
+    const dayUTCJS = timestampToTimeUTC(timestamp)
+    const time = moment('12:30', 'h:mm').format('HH:mm')
+    const time2 = moment(time, 'hmm').format('HH:mm')
+
+    const start = '11:01'
+    const end = '12:00'
+    const result = moment(end, 'hmm').diff(moment(start, 'hmm'))
+    console.log(moment(end, 'hmm').format('HH:mm'))
+    console.log(time2)
+    console.log(time)
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div style={{ float: 'right' }}>
+          The Current Time according to moment
+          is...........................................: {currentTime}
+        </div>
+        <div style={{ float: 'right' }}>
+          The date and time representing timestamp: {timestamp} accorting to
+          moment is: {day.format('YYYY-M-D HH:mm')}
+        </div>
+        <div style={{ float: 'right' }}>
+          The date and time representing timestamp: {timestamp} accorting to
+          moment with .utc() method is: {day.utc().format('YYYY-M-D HH:mm')}
+        </div>
+        <div style={{ float: 'right' }}>
+          The date and time representing timestamp: {timestamp} accorting to JS
+          <code>new Date()</code> is: {dayJS}
+        </div>
+        <div style={{ float: 'right' }}>
+          The date and time representing timestamp: {timestamp} accorting to JS
+          <code>new Date()</code> in UTC is: {dayUTCJS}
+        </div>
+        <div style={{ float: 'right' }}>
+          The Current Time plus 1 hour
+          is........................................: {time}
+        </div>
+        <div style={{ float: 'right' }}>
+          The Current Time plus 1 hour
+          is........................................: {time2}
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
